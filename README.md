281467d98238b3e8c73a81834b007c13709b0d21
Prakhar Srivastav <prakhar1989@gmail.com>
2015-10-07 18:09:39 -0400

    Added a simple test

---
+++ README.md
@@ -0,0 +1,5 @@
+### Hashmap
+
+A naive implementation of a Hash map in Golang.
+
+Nothing interesting to see here... move along. :runner:
--- hashmap_test.go
+++ hashmap_test.go
@@ -1,3 +1,20 @@
 package hashmap
 
 import "testing"
+
+func TestNewHashMap(t *testing.T) {
+	cases := []struct {
+		in, want int
+	}{
+		{10, 10},
+		{20, 20},
+		{0, 0},
+	}
+
+	for _, c := range cases {
+		got := NewHashMap(c.in).size
+		if got != c.want {
+			t.Errorf("NewHashMap(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
